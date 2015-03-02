6042154ad76da9fef0d41b42cc05c9a67eec115a
Josh Nagel <jnagel33@gmail.com>
2015-03-02 14:41:40 -0700

    Add searchBar and tableView. Currently using fake results

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Josh Nagel on 3/2/15.
+//  Copyright (c) 2015 jnagel. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
